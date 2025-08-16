5f6766d318f68efa5a44a2e381512ac9dc17d5f9
Gaurav-Swami <gauravswami0404@gmail.com>
2025-08-17 00:52:29 +0530

    todo

---
+++ convex/todos.ts
@@ -0,0 +1,7 @@
+import { query } from "./_generated/server";
+export const getTodos = query({
+  handler: async (ctx) => {
+    const todos = await ctx.db.query("todos").order("desc").collect();
+    return todos;
+  },
+});
